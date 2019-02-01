a35267e8e618140bdd35d868f13ca9d98d2390c1
mvincent <mvincent@DINFO157>
2019-02-01 15:26:55 +0100

    v0.2

--- prog/src/defaut/TpMobile.java
+++ prog/src/defaut/TpMobile.java
@@ -3,11 +3,15 @@ package defaut;
 import java.awt.*;
 import javax.swing.*;
 
-public class TpMobile
-{
-    public static void main(String[] telsArgs) 
-    {
-	new UneFenetre();
+public class TpMobile{
+    public static void main(String[] telsArgs){
+    	
+    	JFrame fenetre = new UneFenetre();
+    	
+    	fenetre.setResizable(false);
+    	fenetre.setSize(new Dimension(1200, 650));
+    	fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    	fenetre.setVisible(true);
  	
     }
 }
--- prog/src/defaut/UneFenetre.java
+++ prog/src/defaut/UneFenetre.java
@@ -2,17 +2,21 @@ package defaut;
 import java.awt.*;
 import javax.swing.*;
 
-class UneFenetre extends JFrame 
-{
+class UneFenetre extends JFrame {
     UnMobile sonMobile;
     private final int LARG=400, HAUT=250;
     
-    public UneFenetre()
-    {
-	// TODO 
-	// ajouter sonMobile a la fenetre
-	// creer une thread laThread avec sonMobile
-	// afficher la fenetre
-	// lancer laThread 
+    public UneFenetre(){
+		super("fenetre");
+		
+		Container leConteneur = getContentPane();
+		
+		sonMobile = new UnMobile(1200,650);
+		leConteneur.add(sonMobile);
+		
+		Thread laThread = new Thread(sonMobile);
+		laThread.start();
+
+		
     }
 }
