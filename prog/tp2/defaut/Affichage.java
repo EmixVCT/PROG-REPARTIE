666a249657658abc513329571ea1bc1bddbfd748
mvincent <mvincent@DINFO157>
2019-02-08 14:32:01 +0100

    cr

--- prog/tp2/defaut/Affichage.java
+++ prog/tp2/defaut/Affichage.java
@@ -3,22 +3,28 @@ package defaut;
  * 
  */
 import java.io.*;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.io.BufferedReader;
 import java.lang.String;
 
 public class Affichage extends Thread{
-	String texte; 
-
-	public Affichage (String txt){texte=txt;}
+	String texte;
+	static Object excluMut = new Object();
+	
+	public Affichage (String txt){
+		texte=txt;
+		}
 	
 	public void run(){
-
-		for (int i=0; i<texte.length(); i++){
-		    System.out.print(texte.charAt(i));
-		    try {sleep(100);} catch(InterruptedException e){};
+		//semaphoreBinaire semaphore = new semaphoreBinaire(this.);
+		//semaphore.syncWait();
+		
+		synchronized(excluMut){ 
+			for (int i=0; i<texte.length(); i++){
+			    System.out.print(texte.charAt(i));
+			    try {
+			    	sleep(100);
+			    } catch(InterruptedException e){};
+			}
 		}
-
+		//semaphore.syncSignal();
 	}
 }
--- prog/tp2/defaut/semaphoreBinaire.java
+++ prog/tp2/defaut/semaphoreBinaire.java
@@ -1,14 +1,15 @@
 package defaut;
 
 public final class semaphoreBinaire extends semaphore {
-public semaphoreBinaire(int valeurInitiale){
-	super((valeurInitiale != 0) ? 1:0);
-	//System.out.print(valeurInitiale);
-}
-public final synchronized void syncSignal(){
-	super.syncSignal();
-	//System.out.print(valeur);
-	if (valeur>1) valeur = 1;
-}
+	
+	public semaphoreBinaire(int valeurInitiale){
+		super((valeurInitiale != 0) ? 1:0);
+		//System.out.print(valeurInitiale);
+	}
+	public final synchronized void syncSignal(){
+		super.syncSignal();
+		//System.out.print(valeur);
+		if (valeur>1) valeur = 1;
+	}
 }
 
